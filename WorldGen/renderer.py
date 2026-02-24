c0e2f7b4ab61fd38a1c8bf8b3b951c576d930460
Dtar380 <dtar380@icloud.com>
2026-02-24 23:15:43 +0100

    Added biome blend helper to blend colors on biome edges

--- WorldGen/renderer.py
+++ WorldGen/renderer.py
@@ -6,6 +6,7 @@ from matplotlib.colors import ListedColormap, BoundaryNorm, Normalize
 import numpy as np
 import numpy.typing as npt
 from PIL import Image
+from scipy.ndimage import binary_dilation, gaussian_filter  # type: ignore
 
 from .biome import Biome
 
@@ -84,11 +85,43 @@ class Renderer:
         z = np.sin(alt)
         return np.array([x, y, z])
 
+    def _biome_blend(
+        self,
+        colors: npt.NDArray[np.float64],
+        biome_map: npt.NDArray[np.int16],
+        elevation: npt.NDArray[np.float64],
+        sea_level: float = 0.5,
+        radius: int = 10,
+    ) -> npt.NDArray[np.float64]:
+        diff_x = biome_map[:, 1:] != biome_map[:, :-1]
+        diff_y = biome_map[1:, :] != biome_map[:-1, :]
+
+        marine = elevation <= sea_level
+
+        same_type_x = (marine[:, 1:] == marine[:, :-1])
+        same_type_y = (marine[1:, :] == marine[:-1, :])
+
+        border_x = diff_x & same_type_x
+        border_y = diff_y & same_type_y
+
+        borders = np.zeros(biome_map.shape, dtype=bool)
+        borders[:, 1:] |= border_x
+        borders[:, :-1] |= border_x
+        borders[1:, :] |= border_y
+        borders[:-1, :] |= border_y
+
+        dilated = binary_dilation(borders, iterations=radius)
+        blurred = gaussian_filter(colors, sigma=(radius/2, radius/2, 0))
+        colors[dilated] = blurred[dilated]
+
+        return colors
+
     def render(
         self,
         elevation: npt.NDArray[np.float64],
         biome_map: npt.NDArray[np.int16],
         sea_level: float = 0.5,
+        biome_blend: int = 10,
         relief: float = 100.0,
         ambient: float = 0.4,
         sun_altitud: float = -45,
@@ -104,12 +137,11 @@ class Renderer:
         for i, biome in enumerate(self._biomes):
             mask = biome_map == i
             colors[mask] = self._hex_to_rgb(biome.color)
+        colors = self._biome_blend(
+            colors, biome_map, elevation, sea_level, biome_blend
+        )
 
         intensity = ambient + (1 - ambient) * np.maximum(0, np.dot(normals, sun))
         colors *= intensity[:, :, np.newaxis]
-
-        print("sun:", sun)
-        print("normals min/max:", normals.min(), normals.max())
-        print("intensity min/max:", intensity.min(), intensity.max())
 
         return Image.fromarray((colors * 255).astype(np.uint8))
